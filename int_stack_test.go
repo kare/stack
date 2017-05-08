555e42906fc2ed577e03df678c6f7fcb2c3f7730
Kare Nuorteva <kare.nuorteva@me.com>
2017-05-08 15:11:30 +0300

    Fix Peek and Pop on empty stack

--- int_stack_test.go
+++ int_stack_test.go
@@ -9,7 +9,7 @@ func TestIntStackSimple(t *testing.T) {
 	if l != 1 {
 		t.Errorf("Stack Len() should be 1, but was %v", l)
 	}
-	value := stack.Pop()
+	value, _ := stack.Pop()
 	expected := 1
 	if value != expected {
 		t.Errorf("Stack should have returned %v, but returned %v", expected, value)
@@ -27,11 +27,19 @@ func TestIntStackFiveValues(t *testing.T) {
 	}
 
 	for i := 4; i >= 0; i-- {
-		value := stack.Pop()
+		value, _ := stack.Pop()
 		expected := i
 		if value != expected {
 			t.Errorf("Stack should have returned %v, but returned %v", expected, value)
 		}
+	}
+}
+
+func TestIntStackPopEmpty(t *testing.T) {
+	stack := NewInt()
+	zero, err := stack.Pop()
+	if err.Error() != "stack is empty" {
+		t.Errorf("expected peek to return an error, but got %d, %v", zero, err)
 	}
 }
 
@@ -43,5 +51,35 @@ func TestIntStackIsEmpty(t *testing.T) {
 	stack.Push(1)
 	if stack.IsEmpty() {
 		t.Error("Did not expect an empty stack, but it was empty")
+	}
+}
+
+func TestIntStackPeek(t *testing.T) {
+	stack := NewInt()
+	stack.Push(1)
+	one, _ := stack.Peek()
+	if one != 1 {
+		t.Errorf("expected peek to return 1, but got %d", one)
+	}
+}
+
+func TestIntStackPeekEmpty(t *testing.T) {
+	stack := NewInt()
+	zero, err := stack.Peek()
+	if err.Error() != "stack is empty" {
+		t.Errorf("expected peek to return an error, but got %d, %v", zero, err)
+	}
+}
+
+func TestIntStackSlice(t *testing.T) {
+	stack := NewInt()
+	stack.Push(1)
+	stack.Push(2)
+	slice := stack.Slice()
+	if slice[0] != 2 {
+		t.Errorf("expected slice[0] to equal %d", 2)
+	}
+	if slice[1] != 1 {
+		t.Errorf("expected slice[1] to equal %d", 1)
 	}
 }
--- stack.go
+++ stack.go
@@ -1,3 +1,7 @@
 package stack // import "kkn.fi/stack"
 
+import "errors"
+
 //go:generate gends templates.json stack.tmpl
+
+var ErrEmptyStack = errors.New("stack is empty")
