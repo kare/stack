7f65782a05595f894dad34dc96e42b6d7c1b46bb
Kare Nuorteva <kare.nuorteva@me.com>
2024-09-10 23:08:20 +0300

    build: add Go module path to release commit

--- release-git-tag.bash
+++ release-git-tag.bash
@@ -29,7 +29,7 @@ fi
 ./release-validate-semver.bash "$version"
 
 # Create release commit
-git commit --allow-empty --message="release: v${version}"
+git commit --allow-empty --message="release: kkn.fi/stack v${version}"
 git push
 # Create tag
 git tag --message="Release ${version}" --annotate "v${version}"
